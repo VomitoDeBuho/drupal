80f97a442bd80b63fa3b56e0a8356d1975d9f822
VomitoDeBuho <61467108+VomitoDeBuho@users.noreply.github.com>
2021-03-22 02:54:48 +0100

    Update HelloUserController.php

--- hello_user/src/Controller/HelloUserController.php
+++ hello_user/src/Controller/HelloUserController.php
@@ -4,14 +4,14 @@ use Drupal\Core\Controller\ControllerBase;
 class HelloUserController extends ControllerBase {
     public function helloUser($name) {
       #Obrim la taula
-          $table = "<table style='width: 200px; height: 50px; border: 10px solid black'>";
+          $taula = "<table style='width: 200px; height: 50px; border: 10px solid black'>";
       #Fem el for per posar tantes taules com l'usuari ha printat
           for ($j = 1; $j < $name+1; $j++){
-            $table .="<tr style='border: 10px solid black'><td/>$j</tr>";
+            $taula .="<tr style='border: 10px solid black'><td/>$j</tr>";
           }
       #Tanquem la taula i l'imprimim per pantalla
-          $table .= "</table>";
-           return ['#type' => 'inline_template','#template' => $table];
+          $taula .= "</table>";
+           return ['#type' => 'inline_template','#template' => $taula];
     }
 }
 ?>
